29cf6aee9bccf8b378b7af9f988ff68fede4ab47
Ivan Berk <berkoivan@gmail.com>
2020-07-16 00:07:22 +0300

    hw01 done

--- 01-node-basics/contacts.js
+++ 01-node-basics/contacts.js
@@ -7,20 +7,52 @@ const contactsPath = path.join(__dirname, "./db/contacts.json");
 async function listContacts() {
   try {
     const data = await fsPromises.readFile(contactsPath, "utf8");
-    const result = JSON.parse(data);
-    return result.map((item) => ({
-      name: item.name,
-      phone: item.phone,
-    }));
+    return JSON.parse(data);
   } catch (err) {
     console.log(err);
   }
 }
 
-function getContactById(contactId) {}
+async function getContactById(contactId) {
+  try {
+    const data = await fsPromises.readFile(contactsPath, "utf8");
+    const dataParse = JSON.parse(data);
+    return dataParse.find((contact) => contact.id === contactId);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-function removeContact(contactId) {}
+async function removeContact(contactId) {
+  try {
+    const data = await fsPromises.readFile(contactsPath, "utf8");
+    const dataParse = JSON.parse(data);
+    const deleteContact = dataParse.filter(
+      (contact) => contact.id !== contactId
+    );
+    await fsPromises.writeFile(contactsPath, JSON.stringify(deleteContact));
+    return deleteContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-function addContact(name, email, phone) {}
+async function addContact(name, email, phone) {
+  try {
+    const data = await fsPromises.readFile(contactsPath, "utf8");
+    const dataParse = JSON.parse(data);
+    const newContact = {
+      id: dataParse.length + 1,
+      name,
+      email,
+      phone,
+    };
+    dataParse.push(newContact);
+    await fsPromises.writeFile(contactsPath, JSON.stringify(dataParse));
+    return dataParse;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-module.exports = { listContacts };
+module.exports = { listContacts, getContactById, removeContact, addContact };
