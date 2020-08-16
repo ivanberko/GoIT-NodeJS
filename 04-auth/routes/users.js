a61b7baf02e77174964665763df9b3c16f85bc7c
Ivan Berk <berkoivan@gmail.com>
2020-08-17 01:36:39 +0300

    added additional task

--- 04-auth/routes/users.js
+++ 04-auth/routes/users.js
@@ -5,5 +5,11 @@ const usersController = require("../contollers/usersController");
 const validate = require("../validate/validateAuth");
 
 router.post("/current", validate.authorize, usersController.currentUser);
+router.patch(
+  "/subscription",
+  validate.authorize,
+  validate.validateSubscription,
+  usersController.updateSubscription
+);
 
 module.exports = router;
