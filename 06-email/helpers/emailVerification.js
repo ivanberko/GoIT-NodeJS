c54e59dc34d55bcf53ceb9a99c6eefaee4ae7763
Ivan Berk <berkoivan@gmail.com>
2020-08-27 18:41:12 +0300

    fixed

--- 06-email/helpers/emailVerification.js
+++ 06-email/helpers/emailVerification.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 module.exports.mail = (email, link) => ({
   to: email,
-  from: "berko.ivan.kh@gmail.com",
+  from: process.env.MAIL_FROM,
   subject: "and easy to do anywhere, even with Node.js",
   text: "Congratulations, you have successfully verified your mail.",
   html: `
   <p>Follow the link below to confirm your email verification</p>
-  <a href="http://localhost:5000/auth/verify/${link}">http://localhost:5000/auth/verify/${link}</a>`,
+  <a href="http://localhost:${process.env.PORT}/auth/verify/${link}">http://localhost:${process.env.PORT}/auth/verify/${link}</a>`,
 });
